443eb29af545df7725442e10b7b1d67a3676eb87
16 Macbook pro <16macbookpro@16s-macbook-pro.lan>
2021-05-20 20:29:16 -0600

    added default value on filterelement

--- pages/home/filterelement.js
+++ pages/home/filterelement.js
@@ -1,4 +1,4 @@
-const FilterElement = ({item, onFilterBy, filterOptions }) => {
+const FilterElement = ({item = {}, onFilterBy, filterOptions }) => {
     return ( 
         <li onClick={() => onFilterBy(item)} className={`my-2 ${filterOptions[item.key] === item.value  ? "text-blue-400" : ""}`} key={item.value}>{item.value} 
             <a className="text-gray-400"> {item.doc_count}</a>
